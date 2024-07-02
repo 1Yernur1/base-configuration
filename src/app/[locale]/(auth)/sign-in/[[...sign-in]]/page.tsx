b6371122c97a3fcf082f0eb7c07433eae309b3a5
Yernur <shalgymbayev.yernur2001@gmail.com>
2024-07-02 19:19:49 +0500

    alfa-1.0.5 add shadcn

--- src/app/[locale]/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/[locale]/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -6,10 +6,6 @@ import { useClerkTheme } from "@/shared";
 const SignInPage = () => {
 	const clerkTheme = useClerkTheme();
 
-	if (!clerkTheme) {
-		return null;
-	}
-
 	return (
 		<div className="h-screen flex justify-center items-center">
 			<SignIn appearance={{ baseTheme: clerkTheme }} />
--- src/app/[locale]/(root)/page.tsx
+++ src/app/[locale]/(root)/page.tsx
@@ -1,7 +1,10 @@
+import { Switch } from "@/components/ui/switch";
+
 const RootPage = () => {
 	return (
 		<div>
 			<h1>this is root page</h1>
+			<Switch />
 		</div>
 	);
 };
